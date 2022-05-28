a7f38e090c83fc7fd989f540f6c3e2eda975fcd3
sameergross <73777693+sameergross@users.noreply.github.com>
2022-05-28 18:30:23 +0200

    before eating dinner 28.05.2022
    ssasasa

--- main.js
+++ main.js
@@ -4,3 +4,12 @@ m = n.getMonth() + 1;
 d = n.getDate();
 document.getElementById("date").innerHTML = d + "/" + m + "/" + y;
 
+
+function handleclose() {
+    var x = document.getElementById("activemenu");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
