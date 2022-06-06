9312b9eeea8bd0c42210fa31071bd02c1201781a
sameergross <73777693+sameergross@users.noreply.github.com>
2022-06-06 23:39:41 +0200

    final final

--- main.js
+++ main.js
@@ -7,10 +7,10 @@ document.getElementById("date").innerHTML = d + "/" + m + "/" + y;
 //menumobile
 function handleclose() {
     var x = document.getElementById("activemenu");
-    if (x.style.display === "none") {
-        x.style.display = "block";
-    } else {
+    if (x.style.display == "block") {
         x.style.display = "none";
+    } else {
+        x.style.display = "block";
     }
 }
 
